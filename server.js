541fe02dad7168a1861838322faf6d265364ab92
Hitesh <h08rajbhar@gmail.com>
2023-03-16 19:22:57 +0530

    paypal integration

--- server.js
+++ server.js
@@ -29,6 +29,10 @@ app.get('/', (req, res) => {
   res.send('API is running....');
 });
 
+app.get('/api/config/paypal', (req, res) => {
+  res.send(process.env.CLIENT_ID);
+});
+
 // error middlewares
 app.use(notFound);
 app.use(errorHandler);
