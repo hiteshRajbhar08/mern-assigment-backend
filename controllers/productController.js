bd841777c6c530066e5383e40ffc86124647eea4
Hitesh <h08rajbhar@gmail.com>
2023-03-16 19:27:39 +0530

    search query

--- controllers/productController.js
+++ controllers/productController.js
@@ -8,7 +8,16 @@ const asyncHandler = require('express-async-handler');
  * @access  public 
  ------------------------------------------------*/
 const getProducts = asyncHandler(async (req, res) => {
-  const products = await Product.find({});
+  const keyword = req.query.keyword
+    ? {
+        name: {
+          $regex: req.query.keyword,
+          $options: 'i',
+        },
+      }
+    : {};
+
+  const products = await Product.find({ ...keyword });
   res.json(products);
 });
 
